7a9eebd1876f0f2c60972307e8d7be02f456dabb
RuthMaria <ruth-15rms@hotmail.com>
2025-03-25 20:23:03 -0300

    feat: adiciona usuário

---
+++ UsuariosAPI/Data/UsuarioDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Data;
+
+public class UsuarioDbContext : IdentityDbContext<Usuario>
+{
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=usuariodb;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    public UsuarioDbContext(DbContextOptions<UsuarioDbContext> opts) : base(opts) { }
+}
--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using UsuariosAPI.Data;
+using UsuariosAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UsuarioDbContext>();
 
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders(); 
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
